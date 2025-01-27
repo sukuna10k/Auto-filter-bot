9e00959b843dd6dda5dbb479e8b577b09d7e7ae2
sukuna10k <abdoulmadjidkingceymarkouma@gmail.com>
2025-01-27 17:44:34 +0000

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -6,29 +6,29 @@ from datetime import datetime
 
 
 @Client.on_message(filters.command('id'))
-async def showid(client, message):
-    chat_type = message.chat.type
-    replied_to_msg = bool(message.reply_to_message)
-    if replied_to_msg:
-        return await message.reply_text(f"""The forwarded message channel {replied_to_msg.chat.title}'s id is, <code>{replied_to_msg.chat.id}</code>.""")
-    if chat_type == enums.ChatType.PRIVATE:
-        await message.reply_text(f'★ User ID: <code>{message.from_user.id}</code>')
+async def afficher_id(client, message):
+    type_chat = message.chat.type
+    message_repondu = bool(message.reply_to_message)
+    if message_repondu:
+        return await message.reply_text(f"""L'ID du message transféré provenant de {message_repondu.chat.title} est : <code>{message_repondu.chat.id}</code>.""")
+    if type_chat == enums.ChatType.PRIVATE:
+        await message.reply_text(f'★ ID Utilisateur : <code>{message.from_user.id}</code>')
 
-    elif chat_type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
-        await message.reply_text(f'★ Group ID: <code>{message.chat.id}</code>')
+    elif type_chat in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
+        await message.reply_text(f'★ ID Groupe : <code>{message.chat.id}</code>')
 
-    elif chat_type == enums.ChatType.CHANNEL:
-        await message.reply_text(f'★ Channel ID: <code>{message.chat.id}</code>')
+    elif type_chat == enums.ChatType.CHANNEL:
+        await message.reply_text(f'★ ID Canal : <code>{message.chat.id}</code>')
 
 
 @Client.on_message(filters.command('speedtest') & filters.user(ADMINS))
-async def speedtest(client, message):
-    #from - https://github.com/weebzone/WZML-X/blob/master/bot/modules/speedtest.py
-    msg = await message.reply_text("Initiating Speedtest...")
+async def test_de_vitesse(client, message):
+    # Source : https://github.com/weebzone/WZML-X/blob/master/bot/modules/speedtest.py
+    msg = await message.reply_text("Lancement du Speedtest...")
     try:
         speed = Speedtest()
     except ConfigRetrievalError:
-        await msg.edit("Can't connect to Server at the Moment, Try Again Later !")
+        await msg.edit("Impossible de se connecter au serveur pour le moment, réessayez plus tard !")
         return
     speed.get_best_server()
     speed.download()
@@ -36,30 +36,30 @@ async def speedtest(client, message):
     speed.results.share()
     result = speed.results.dict()
     photo = result['share']
-    text = f'''
-➲ <b>SPEEDTEST INFO</b>
-┠ <b>Upload:</b> <code>{get_size(result['upload'])}/s</code>
-┠ <b>Download:</b>  <code>{get_size(result['download'])}/s</code>
-┠ <b>Ping:</b> <code>{result['ping']} ms</code>
-┠ <b>Time:</b> <code>{datetime.strptime(result['timestamp'], "%Y-%m-%dT%H:%M:%S.%fZ").strftime("%Y-%m-%d %H:%M:%S")}</code>
-┠ <b>Data Sent:</b> <code>{get_size(int(result['bytes_sent']))}</code>
-┖ <b>Data Received:</b> <code>{get_size(int(result['bytes_received']))}</code>
+    texte = f'''
+➲ <b>INFORMATIONS SUR LE SPEEDTEST</b>
+┠ <b>Téléchargement :</b> <code>{get_size(result['upload'])}/s</code>
+┠ <b>Débit :</b>  <code>{get_size(result['download'])}/s</code>
+┠ <b>Ping :</b> <code>{result['ping']} ms</code>
+┠ <b>Heure :</b> <code>{datetime.strptime(result['timestamp'], "%Y-%m-%dT%H:%M:%S.%fZ").strftime("%Y-%m-%d %H:%M:%S")}</code>
+┠ <b>Données envoyées :</b> <code>{get_size(int(result['bytes_sent']))}</code>
+┖ <b>Données reçues :</b> <code>{get_size(int(result['bytes_received']))}</code>
 
-➲ <b>SPEEDTEST SERVER</b>
-┠ <b>Name:</b> <code>{result['server']['name']}</code>
-┠ <b>Country:</b> <code>{result['server']['country']}, {result['server']['cc']}</code>
-┠ <b>Sponsor:</b> <code>{result['server']['sponsor']}</code>
-┠ <b>Latency:</b> <code>{result['server']['latency']}</code>
-┠ <b>Latitude:</b> <code>{result['server']['lat']}</code>
-┖ <b>Longitude:</b> <code>{result['server']['lon']}</code>
+➲ <b>SERVEUR DE SPEEDTEST</b>
+┠ <b>Nom :</b> <code>{result['server']['name']}</code>
+┠ <b>Pays :</b> <code>{result['server']['country']}, {result['server']['cc']}</code>
+┠ <b>Sponsor :</b> <code>{result['server']['sponsor']}</code>
+┠ <b>Latence :</b> <code>{result['server']['latency']}</code>
+┠ <b>Latitude :</b> <code>{result['server']['lat']}</code>
+┖ <b>Longitude :</b> <code>{result['server']['lon']}</code>
 
-➲ <b>CLIENT DETAILS</b>
-┠ <b>IP Address:</b> <code>{result['client']['ip']}</code>
-┠ <b>Latitude:</b> <code>{result['client']['lat']}</code>
-┠ <b>Longitude:</b> <code>{result['client']['lon']}</code>
-┠ <b>Country:</b> <code>{result['client']['country']}</code>
-┠ <b>ISP:</b> <code>{result['client']['isp']}</code>
-┖ <b>ISP Rating:</b> <code>{result['client']['isprating']}</code>
+➲ <b>DÉTAILS DU CLIENT</b>
+┠ <b>Adresse IP :</b> <code>{result['client']['ip']}</code>
+┠ <b>Latitude :</b> <code>{result['client']['lat']}</code>
+┠ <b>Longitude :</b> <code>{result['client']['lon']}</code>
+┠ <b>Pays :</b> <code>{result['client']['country']}</code>
+┠ <b>Fournisseur d'accès :</b> <code>{result['client']['isp']}</code>
+┖ <b>Évaluation du fournisseur :</b> <code>{result['client']['isprating']}</code>
 '''
-    await message.reply_photo(photo=photo, caption=text)
+    await message.reply_photo(photo=photo, caption=texte)
     await msg.delete()
