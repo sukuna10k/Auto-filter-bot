39a511c8998d143c5022eabf9f9a54c79a772da4
sukuna10k <abdoulmadjidkingceymarkouma@gmail.com>
2025-02-22 20:35:55 +0000

    Update info.py

--- info.py
+++ info.py
@@ -45,7 +45,7 @@ else:
     ADMINS = [int(admins) for admins in ADMINS.split()]
 
 # Channels
-INDEX_CHANNELS = [int(index_channels) if index_channels.startswith("-") else index_channels for index_channels in environ.get('INDEX_CHANNELS', '-1002491166640 -1002143967853 -1002488233862 -1002241895434 -1002178700944').split()]
+INDEX_CHANNELS = [int(index_channels) if index_channels.startswith("-") else index_channels for index_channels in environ.get('INDEX_CHANNELS', '-1002491166640 -1002143967853 -1002488233862 -1002241895434 -1002178700944 -1002450406603').split()]
 if len(INDEX_CHANNELS) == 0:
     print('Info - INDEX_CHANNELS is empty')
 LOG_CHANNEL = environ.get('LOG_CHANNEL', '-1002376378205')
